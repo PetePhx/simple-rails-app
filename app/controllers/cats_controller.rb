ba91cbb802744f3893986b0caa04f2af24351f13
PetePhx <pete0phx@yandex.com>
2018-10-21 11:39:16 -0700

    cats routes/model/controller/migrate

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,21 @@
+class CatsController < ApplicationController
+  def index
+    render json: Cat.all
+  end
+
+  def show
+    render json: Cat.find(params[:id])
+  end
+
+  def create
+    # POST /cats
+    # { "cat": { "name": "Sally" } }
+    cat = Cat.new(name: params[:cat][:name])
+    if cat.save
+      render json: cat
+    else
+      render json: cat.errors.full_messages, status: :unprocessable_entity
+      # raise "CatError"
+    end
+  end
+end
